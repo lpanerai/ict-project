6cbc52355b4353d418c2be2af8149cbb8f091b10
lpanerai <l.panerai@campus.unimib.it>
2025-01-06 19:56:50 +0100

    test main.py

--- App/main.py
+++ App/main.py
@@ -1,45 +1,95 @@
-from face_enrollment import enroll_user
-from face_reco import recognize_face_live
+from utils import *
 import os
 import glob
 import argparse
+from silero_vad import get_speech_timestamps
 
-def find_image_file(image_path, username):
-    """Trova un file immagine per un utente specifico."""
-    possible_files = glob.glob(os.path.join(image_path, f"{username}.*"))
-    return possible_files[0] if possible_files else None
+#Speaker Rec.
+#Comandi per esecuzione:
+#Eseguire il server in Bash: cmd --> node server.js
+SERVER_URL = "http://localhost:3000/"
 
-def main():
-    # Configurazione degli argomenti da riga di comando
-    parser = argparse.ArgumentParser(description="Sistema di riconoscimento facciale.")
-    parser.add_argument('mode', choices=['1', '2'], help="Seleziona la modalità: 1 per registrazione, 2 per riconoscimento live.")
-    parser.add_argument('--username', help="Nome utente da registrare (necessario in modalità 1).")
-    parser.add_argument('--image_path', help="Percorso della cartella contenente l'immagine da registrare (necessario in modalità 1).")
-    
-    args = parser.parse_args()
+#MAIN -- Loop Vocale
+#Configurazione microfono
+SAMPLE_RATE = 16000  #Silero VAD funziona con 16 kHz
+DURATION_VAD = 3         #Ascolta per 3 secondi
+DURATION_VR = 5          #Ascolta per 5 secondi
+DURATION_EN = 35         #Ascolta per 30 secondi
+c = 0                    #Contatore SV
+exit_flag = True         #Metodo per terminare il ciclo
 
-    if args.mode == '1':
-        if not args.username or not args.image_path:
-            print("Errore: Per la modalità 1 è necessario specificare sia 'username' che 'image_path'.")
-            return
-        
-        image_file = find_image_file(args.image_path, args.username)
-        if image_file is None:
-            print(f"Errore: immagine per '{args.username}' non trovata in '{args.image_path}'.")
-            return
+model, utils = torch.hub.load(
+    repo_or_dir='snakers4/silero-vad',
+    model='silero_vad',
+    source='github'
+    )
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
+
+model_save_path = "C:\\Users\\4k\\Documents\\Università\\2°anno\\ict-project\\Models\\silero_vad_model.pth"
+torch.save(model.state_dict(), model_save_path)
+silero_vad = model.to(device)
+
+def pipeline():
+    print(device)
+    while True:
+            print("Microfono connesso")
+
+            audio = listen_for_audio(DURATION_VAD, SAMPLE_RATE)
+            if(VAD(audio,silero_vad,SAMPLE_RATE)):
+                print("VAD: Utente sta parlando")
+                if(speekerRecognition()):
+                    print("SpeakerRecognition: Utente riconosciuto")
+                    if(faceRecognition()):
+                        print("FaceRecognition: Utente riconosciuto")
+                    else:
+                        print("FaceRecognition: Utente non riconosciuto")
+                else:
+                    print("SpeakerRecognition: Utente non riconosciuto")
+            else:
+                print("VAD: Utente non sta parlando")
+
+def VAD(audio,silero_vad,SAMPLE_RATE):
+    vad_detect(audio, silero_vad, SAMPLE_RATE)
+	
+def speekerRecognition():
+    audio_sv = listen_for_audio(DURATION_VR, SAMPLE_RATE)
+    sd.wait()
+    temp_filename = "temp_audio.wav"
+    save_audio_to_file(audio_sv, temp_filename, SAMPLE_RATE)
+
+    #Invio al server per identificazione
+    print("\n------------------------------------------")
+    print("Invio al server per identificazione...")
+    print("------------------------------------------")
+    response = send_audio_to_server(temp_filename, SERVER_URL + "/identify")
+
+    print(f"\nRisposta del server: {response}")
+    speaker_name, score, Idy = parse_server_response(response)
+    print(f"Score: {score}, Idy: {Idy}")
+
+    if Idy == "Allowed":
+        if score >= 0.5:  # Soglia di riconoscimento
+            print("\n------------------------------------------")
+            print(f"Accesso consentito! Benvenuto, {speaker_name}.")
+            print("------------------------------------------")
+            return True
         else:
-            print(f"Immagine trovata: {image_file}")
+            print("\n------------------------------------------")
+            print("Accesso negato! Utente non riconosciuto.")
+            print("------------------------------------------")
+            return False
+    else:
+           return False
 
-        # Passa il percorso completo del file immagine
-        success = enroll_user(args.username, image_file)
-        if success:
-            print(f"Registrazione completata per {args.username}.")
-        else:
-            print(f"Errore nella registrazione per {args.username}.")
+def faceRecognition():
+    recognize_face_live(threshold=0.6)
 
-    elif args.mode == '2':
-        print("Avvio del riconoscimento facciale live...")
-        recognize_face_live()
+def voice_enrollment():
+	return True
+
+def face_enrollment():
+    enroll_user("Leonardo", "Dataset/People/Leonardo/Photos/Leonardo.jpg")
 
 if __name__ == "__main__":
-    main()
+    pipeline()
